5962958da9f44344f924189e57575e596b39d5d8
Grant <gwensignak@gmail.com>
2019-08-09 12:13:45 -0600

    database is updating however, getting user_id undefined error

--- server/controllers/authControl.js
+++ server/controllers/authControl.js
@@ -3,17 +3,22 @@ const bcrypt = require('bcryptjs')
 module.exports = {
     register: async (req, res) => {
         const db = req.app.get('db')
-        const { username, password } = req.body
+        const { user_id, username, password, profile_pic } = req.body
         console.log('username: ', username)
         console.log('')
         const user = await db.find_user([username])
         if (user.length > 0) {
             return res.status(400).send('Username Taken')
         }
+        console.log('')
+        console.log('user: ', user)
         const salt = bcrypt.genSaltSync(10)
         const hash = bcrypt.hashSync(password, salt)
-        const newUser = await db.insert_helo_user(username, password)
+        const newUser = await db.insert_helo_user(username, password, profile_pic)
+        console.log('')
         console.log('newUser: ', newUser)
+        console.log('')
+        console.log('user_id: ', user_id)
         db.insert_hash({ hash, user_id: newUser[0].user_id })
             .then(() => {
                 console.log('hash: ', hash)
