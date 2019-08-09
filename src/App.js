f54faf8673de1f9431a685f3f0ec05c56550eff0
Grant <gwensignak@gmail.com>
2019-08-09 09:30:59 -0600

    src folders set up complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './App.css';
+import Auth from './components/Auth/Auth'
+import Dashboard from './components/Dashboard/Dashboard'
+import Form from './components/Form/Form'
+import Nav from './components/Nav/Nav'
+import Post from './components/Post/Post'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <Auth />
+      <Dashboard />
+      <Form />
+      <Post />
     </div>
   );
 }
